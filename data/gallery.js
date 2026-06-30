73b515de6e3f5d49e3576fa816e71c584a6fa922
cawralaali33-sudo <cawralaali33@gmail.com>
2026-06-30 04:07:51 -0700

    Creator Challenge update

--- data/gallery.js
+++ data/gallery.js
@@ -5,10 +5,10 @@ export const galleryImages = [
    { src: "/gallery/qanad2.jpeg", title: "Reception", category: "Reception" },
   { src: "/gallery/qanad2.jpeg", title: "24/7 Locker Service", category: "Lockers" },
   { src: "/gallery/likenew.jpeg", title: "Likenew", category: "Likenew" },
-  { src: "/gallery/Likenew.jpeg", title: "Likenew", category: "Likenew" },
+  { src: "/gallery/boor.jpeg", title: "Likenew", category: "Likenew" },
   { src: "/gallery/xarunta.jpeg", title: "lockers", category: "lockers" },
   { src: "/gallery/Likenew.jpeg", title: "Machines", category: "Machines" },
   { src: "/gallery/likenew.jpeg", title: "Professional Team", category: "Team" },
   { src: "/gallery/qanad2.jpeg", title: "Likenew", category: "Likenew" },
-  { src: "/gallery/qanad1.jpeg", title: "Clean Packaging", category: "Shoes" },
+  { src: "/gallery/kabo.jpeg", title: "Clean Packaging", category: "Shoes" },
 ];
